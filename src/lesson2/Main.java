8d1920cebdd3454697b5e52050c2b773a0f83f69
cathnech <catherine.nech@gmail.com>
2020-10-26 14:46:59 +0300

    lesson2 дз

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -4,49 +4,40 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-	public static String[][] arr;
+	public static void main(String[] args) {
+		int res = 0;
+		Scanner sc = new Scanner(System.in);
 
-		public static void main(String[] args) {
-
-			int res = 0;
-			Scanner sc = new Scanner(System.in);
-
-			System.out.println("Введите размерность массива: ");
-			int num1 = sc.nextInt();
-			int num2 = sc.nextInt();
-			String[][] arr = new String[num1][num2];
-
-			try {
-				checkArraySize(num1);
-				checkArraySize(num2);
-			} catch (MyArraySizeException e) {
-				e.printStackTrace();
-				System.out.println(e.getNum());
-				return;
+		System.out.println("Введите размерность массива: ");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		String[][] arr = new String[num1][num2];
+		System.out.println("Заполните массив: ");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.printf("Строка %d столбец %d \n",i+1,j+1);
+				arr[i][j] = sc.next();
 			}
+		}
+		checkAllandCalculate(arr);
+	}
 
-			System.out.println("Заполните массив: ");
 
-			for (int i = 0; i < arr.length; i++) {
-				for (int j = 0; j < arr[i].length; j++) {
-					System.out.printf("Строка %d столбец %d \n",i+1,j+1);
-					arr[i][j] = sc.next();
-
-					try {
-						int x = Integer.parseInt(arr[i][j]);
-						res += x;
-					} catch (NumberFormatException e) {
-						throw new MyArrayDataException("Элемент не является числом", i, j);
-					}
-
+	public static void checkAllandCalculate(String[][] array) throws MyArraySizeException,MyArrayDataException{
+		int res = 0;
+		if (array.length !=4) {
+			throw new MyArraySizeException("Размерность массива должна быть 4x4", array.length);
+		}
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				try {
+					int x = Integer.parseInt(array[i][j]);
+					res += x;
+				} catch (NumberFormatException e) {
+					throw new MyArrayDataException("Элемент не является числом", i, j);
 				}
 			}
+		}
 			System.out.println("Сумма массива:" + res);
 		}
-		public static void checkArraySize(int n) throws MyArraySizeException {
-			if (n != 4){
-				throw new MyArraySizeException("Размерность массива должна быть 4x4", n);
-			}
-		}
-
 }
